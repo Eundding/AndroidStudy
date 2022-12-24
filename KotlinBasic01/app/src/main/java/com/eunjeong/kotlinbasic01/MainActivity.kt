4ae0ebc6fc12ca870ea4cf4da7010f6e175d25a7
Eundding <dkan9634@naver.com>
2022-12-24 14:12:19 +0900

    Btn add

--- KotlinBasic01/app/src/main/java/com/eunjeong/kotlinbasic01/MainActivity.kt
+++ KotlinBasic01/app/src/main/java/com/eunjeong/kotlinbasic01/MainActivity.kt
@@ -2,10 +2,15 @@ package com.eunjeong.kotlinbasic01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.eunjeong.kotlinbasic01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        
     }
 }
