f1ac19c1bf0a0cd819ec69f94e3042b358ea9bd3
Eundding <dkan9634@naver.com>
2022-12-24 14:39:38 +0900

    Toast

--- KotlinBasic01/app/src/main/java/com/eunjeong/kotlinbasic01/MainActivity.kt
+++ KotlinBasic01/app/src/main/java/com/eunjeong/kotlinbasic01/MainActivity.kt
@@ -2,6 +2,8 @@ package com.eunjeong.kotlinbasic01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.eunjeong.kotlinbasic01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +12,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        viewBinding.clickBtn.setOnClickListener {
+            Log.d("MainActivity", "Click btn")
+        }
+        viewBinding.smallBtn.setOnClickListener {
+            Toast.makeText(this, "Click Small Btn", Toast.LENGTH_SHORT).show()
+        }
 
 
     }
