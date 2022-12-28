3344c0732dc0dc773bbc94ea4c156e067b02c27f
Eundding <dkan9634@naver.com>
2022-12-28 22:04:45 +0900

    finish() 이용=> 현재화면 종료, 이전화면 복귀

--- Intent/app/src/main/java/com/eunjeong/intent/MainActivity.kt
+++ Intent/app/src/main/java/com/eunjeong/intent/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.eunjeong.intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.eunjeong.intent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.movieToOtherBtn.setOnClickListener {
+            //다른 화면으로 이동
+            //Intent(출발지, 도착지)
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
---
+++ Intent/app/src/main/java/com/eunjeong/intent/OtherActivity.kt
@@ -0,0 +1,23 @@
+package com.eunjeong.intent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eunjeong.intent.databinding.ActivityMainBinding
+import com.eunjeong.intent.databinding.ActivityOtherBinding
+
+class OtherActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityOtherBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivityOtherBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.moveToPrevBtn.setOnClickListener {
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+            finish() // 지금 화면 꺼주세요
+            //넘어갈때는 intent 되돌아갈때는 finish 활용
+        }
+    }
+}
