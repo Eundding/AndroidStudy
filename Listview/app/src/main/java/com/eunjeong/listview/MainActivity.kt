8424ea73183c3068f9c9118f4cfbd4d0a87a5a3d
Eundding <dkan9634@naver.com>
2022-12-31 00:51:00 +0900

    Listview click, longClick event

--- Listview/app/src/main/java/com/eunjeong/listview/MainActivity.kt
+++ Listview/app/src/main/java/com/eunjeong/listview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.eunjeong.listview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.eunjeong.listview.adapters.StudentAdapter
 import com.eunjeong.listview.databinding.ActivityMainBinding
 import com.eunjeong.listview.datas.Student
@@ -30,6 +31,20 @@ class MainActivity : AppCompatActivity() {
         mAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)
 
         viewBinding.studentListView.adapter = mAdapter
+
+
+        viewBinding.studentListView.setOnItemClickListener { parent, view, position, id ->
+            val clickedStudent = mStudentList[position]
+            Toast.makeText(this, "${clickedStudent.name}이 클릭 됨", Toast.LENGTH_SHORT).show()
+        }
+
+        viewBinding.studentListView.setOnItemLongClickListener { parent, view, position, id ->
+            val longClickedStudent = mStudentList[position]
+            Toast.makeText(this, "${longClickedStudent.name}이 롱클릭 됨", Toast.LENGTH_SHORT).show()
+            return@setOnItemLongClickListener true
+        }
+
+        
 
     }
 }
