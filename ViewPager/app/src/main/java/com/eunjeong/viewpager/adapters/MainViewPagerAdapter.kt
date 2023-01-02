1803ca4b1880f801349756ecddec03f462cac762
Eundding <dkan9634@naver.com>
2023-01-02 11:19:22 +0900

    Change Tablaout text color, indicator color

--- ViewPager/app/src/main/java/com/eunjeong/viewpager/adapters/MainViewPagerAdapter.kt
+++ ViewPager/app/src/main/java/com/eunjeong/viewpager/adapters/MainViewPagerAdapter.kt
@@ -10,14 +10,19 @@ import com.eunjeong.viewpager.fragments.ThirdFragment
 class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
     //페이지별 제목 설정
     override fun getPageTitle(position: Int): CharSequence? {
-        if (position == 0){
-            return "첫번째"
-        }
-        else if (position == 1){
-            return "두번쨰"
-        }
-        else{
-            return "세번째"
+//        if (position == 0){
+//            return "첫번째"
+//        }
+//        else if (position == 1){
+//            return "두번쨰"
+//        }
+//        else{
+//            return "세번째"
+//        }
+        return when(position){
+            0 -> "첫번째"
+            1->"두번째"
+            else -> "세번째"
         }
     }
 
@@ -26,14 +31,19 @@ class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
     }
 
     override fun getItem(position: Int): Fragment {
-        if (position == 0){
-            return FirstFragment()
-        }
-        else if (position == 1){
-            return SecondFragment()
-        }
-        else{
-            return ThirdFragment()
+//        if (position == 0){
+//            return FirstFragment()
+//        }
+//        else if (position == 1){
+//            return SecondFragment()
+//        }
+//        else{
+//            return ThirdFragment()
+//        }
+        return when(position){
+            0 -> FirstFragment()
+            1 -> SecondFragment()
+            else -> ThirdFragment()
         }
     }
 
