d53f235a8b1d66b9887503720d24af0905df1dd9
Eundding <dkan9634@naver.com>
2023-01-03 11:43:30 +0900

    Add Library dependency

---
+++ Library_IMG/app/src/main/java/com/eunjeong/library_img/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.eunjeong.library_img
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
