954b6d7ee56ef759ace66d082a9274478ef196da
Eundding <dkan9634@naver.com>
2023-01-03 20:02:09 +0900

    PhotoView, CircleImageView

--- Library_IMG/app/src/main/java/com/eunjeong/library_img/MainActivity.kt
+++ Library_IMG/app/src/main/java/com/eunjeong/library_img/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.eunjeong.library_img
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ViewAnimator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        profileImg.setOnClickListener {
+            val mIntent = Intent(this, ViewProfileActivity::class.java)
+            startActivity(mIntent)
+        }
+
+
     }
 }
---
+++ Library_IMG/app/src/main/java/com/eunjeong/library_img/ViewProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.eunjeong.library_img
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_profile)
+    }
+}
