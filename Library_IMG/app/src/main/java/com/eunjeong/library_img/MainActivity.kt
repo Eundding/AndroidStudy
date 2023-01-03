31d23557a9f28504e4510f3b874b1bf89062ce92
Eundding <dkan9634@naver.com>
2023-01-03 20:18:35 +0900

    Glide

--- Library_IMG/app/src/main/java/com/eunjeong/library_img/MainActivity.kt
+++ Library_IMG/app/src/main/java/com/eunjeong/library_img/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ViewAnimator
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +12,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //프로필 크게 보는 액티비티로 이동
         profileImg.setOnClickListener {
             val mIntent = Intent(this, ViewProfileActivity::class.java)
             startActivity(mIntent)
         }
+
+        // 인터넷에 있는 이미지(https://로 시작)를 이미지뷰에 대입
+        Glide.with(this).load("https://yozm.wishket.com/media/news/906/image002.jpg").into(img1)
+
 
 
     }
